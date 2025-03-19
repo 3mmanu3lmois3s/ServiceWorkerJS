61349ef29b22583999ad2959c952b1474613d16f
emmanuel.mellado <emmanuel.mellado@external.securitasdirect.es>
2025-03-19 10:53:17 +0100

    fix route

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Register the service worker
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js').then(function(registration) {
+    navigator.serviceWorker.register('./ServiceWorkerJS/sw.js').then(function(registration) {
         console.log('Service Worker registered with scope:', registration.scope);
     }).catch(function(error) {
         console.log('Service Worker registration failed:', error);
